774c91a908c1f8f03f542821aac78d9ac9638a0c
Adam Nowland <acnowland@gmail.com>
2023-02-06 16:48:58 -0800

    Initial Commit

---
+++ tipApp/tipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipAppApp.swift
+//  tipApp
+//
+//  Created by Adam Nowland on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct tipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
